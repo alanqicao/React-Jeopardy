fbbd6f984d68927fe03e62bf160b53b4073834d7
Qi Cao <alanqicao@gmail.com>
2020-03-26 16:18:28 -0600

    init

--- app/controllers/api/categorys_controller.rb
+++ app/controllers/api/categorys_controller.rb
@@ -1,16 +1,42 @@
 class Api::CategorysController < ApplicationController
+    before_action :set_category, 
+
   def index
+    render json: Category.all
   end
 
   def show
+    render json: @category
   end
 
   def create
+    category = Category.new(category_params)
+
+    if category.save
+      render json: category
+    else
+      render json: category.errors,status: 422
+    end
   end
 
   def update
+    if @category.update(category_params)
+      render json: @category
+    else
+      render json: @category.errors, status: 422
   end
 
   def destroy
+    @category.destroy
   end
+
+  private
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    def category_params
+      params.requir(:category).permit(:name)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :categorys do
+      resources :cards
+    end
+    resources :cards do
+      resources :questions
+    end
+  end
+
+  get '*other', to: 'static#index'
+  
 end
