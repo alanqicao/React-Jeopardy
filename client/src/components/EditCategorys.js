eb3919c6d8a0b9884dde20d9ce423d630805a7d1
Qi Cao <alanqicao@gmail.com>
2020-03-27 01:42:57 -0600

    delete

--- client/src/components/EditCategorys.js
+++ client/src/components/EditCategorys.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, } from 'react';
 import CategorysForm from "./CategorysForm"
-import { Header, RowCenter, Title, Button, Segment, List } from "semantic-ui-react";
+import { Header,  Button, List } from "semantic-ui-react";
 import axios from 'axios'
 import { Link, } from "react-router-dom";
 import Category from "./Category"
@@ -10,8 +10,8 @@ import Category from "./Category"
 const EditCategory = () => {
 
   const [categorys, setName] = useState([]);
-  const [cards, setCard] = useState ([]);
-  const [questions,setQuestion] = useState ([])
+  // const [cards, setCard] = useState ([]);
+  // const [questions,setQuestion] = useState ([])
   const [showForm,setShowForm] = useState(false)
 
   useEffect(() => {
@@ -29,7 +29,7 @@ const EditCategory = () => {
   const renderCategory = () => {
     return categorys.map(categorys => (
       <div style={{ margin: "0em 1em" }}>
-        <Category {...categorys} editCategory={editCategory} deleteCategory={deleteCategory}/>
+        <Category id = {categorys.id} {...categorys} editCategory={editCategory} deleteCategory={deleteCategory}/>
       </div>
     )
 
@@ -57,7 +57,7 @@ const EditCategory = () => {
     axios
     .delete(`/api/categorys/${id}`)
     .then(res => {
-         setName.filter(categorys => categorys.id !== id)
+        res = setName.filter(categorys => categorys.id !== id)
     })
   }
 
