8ba9cf2c9c5fb5ff4638efd57e4cf7b26286befe
Brianna Bell <bbell9563@gmail.com>
2020-03-27 11:57:17 -0600

    merge conflicts resolved

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,10 @@ import Home from './components/Home';
 import About from './components/About';
 import NoMatch from './components/NoMatch';
 import { Container, } from "semantic-ui-react";
-<<<<<<< HEAD
 import ConnectedPlayJeopardy from './components/PlayJeopardy';
-=======
 import EditCategory from './components/EditCategorys';
 import Navbar from './components/Navbar'
 import CategoryView from './components/CategoryView'
->>>>>>> eb3919c6d8a0b9884dde20d9ce423d630805a7d1
 
 const App = () => (
   <Fragment>
@@ -19,12 +16,9 @@ const App = () => (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
-<<<<<<< HEAD
       <Route exact path='/playjeopardy' component={ConnectedPlayJeopardy} />
-=======
       <Route exact path="/EditCategorys" component={EditCategory} />
       <Route exact path="/categorys/:id" component={CategoryView} />
->>>>>>> eb3919c6d8a0b9884dde20d9ce423d630805a7d1
       <Route component={NoMatch} />
     </Switch>
   </Container>
